98e0bc61a7d1b239840e6fef29b320ce14d44229
Brandon Schwartz <bschwartz@rivacrmintegration.com>
2017-04-27 10:48:38 -0600

    initial commit for basic structure review

---
+++ cloud_test_automation/queue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class queue
+    {
+        test[] testQueue;
+        queue() {
+
+        }
+
+        next() {
+
+        }
+
+
+    }
+}
